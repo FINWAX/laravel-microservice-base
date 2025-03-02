3fa2fb6c7b0a8ca887dd5fe0980de01b7e50b48a
Pavel E. <deusagnis@gmail.com>
2025-03-02 23:02:57 +0300

    Update web routes.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
+// Auto reset cache for views in dev environment
+if (config('app.env', 'production') != 'production') {
+    Artisan::call('view:clear');
+}
+
 Route::get('/', function () {
-    return view('welcome');
+    return Response::make(config('app.name').' is available.');
 });
